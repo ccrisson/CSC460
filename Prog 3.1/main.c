a7d5a40a6bdb36fa509282b2bff89865c7752fd9
Zidlan1O <45948180+Zidlan1O@users.noreply.github.com>
2019-03-19 20:59:03 -0400

    Add files via upload

---
+++ Prog 3.1/main.c
@@ -0,0 +1,30 @@
+
+
+#include "file_util.h"
+
+
+char inFile[MAX_CHAR], outFile[MAX_CHAR],tmpName[MAX_CHAR],c='\0',choice='\0';
+			// typedef to token for easier reference
+int tokNum=0,lexErrors=0,synErrors=0,counter=0;
+int main (int argc, char *argv[])
+{
+	int i=0;
+         if(getInFile(argc, argv,&input));
+		{
+		 				
+			if(getOutFile(argc, argv, &output, &listing, &tmp)) //call function for output file opening
+			{
+			
+				 scanner(input,output,listing);
+				 closeFiles( input,  output,  listing,  tmp); //call function to close any open files
+				 printf("\n There were %d tokens total",tokNum);
+				 printf("\n There were %d Lexical errors\n",lexErrors);
+				
+			};
+			
+		}
+	
+  printf("\n\tHAVE A NICE DAY \n");
+  system ("Pause");
+  return 0;
+}
