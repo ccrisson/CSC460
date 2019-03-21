880e310ea38c3b9b6f9dea48006da40fcbe038b6
Zidlan1O <45948180+Zidlan1O@users.noreply.github.com>
2019-03-21 10:50:42 -0400

    Update file_util.h

--- Prog 3.1/file_util.h
+++ Prog 3.1/file_util.h
@@ -12,25 +12,25 @@
 #define MAX_CHAR 30
 #define MAXBUFF 200
 typedef struct tokn{
-	const char *str;  //the actual token
-	int len;			//the length of the token
-	int num;			//the enumeration of the token
-}token;		
+	const char *str;  	
+	int len;		//Length of the token
+	int num;		//Enumeration of the token
+}token;				//Reference
 FILE *input,*output,*listing,*tmp,*back_up;
 int tokNum;
-int getInFile(int arg_count,  char * argv[],FILE **input); 			//reads user input checks if  input file exists and opens it
-int getOutFile(int arg_count,  char * argv[], FILE **output, FILE **listing, FILE **tmp); //gets user input  checks if output file exists, or opens a file input.OUT
-void copy_backup(char *outFile);  // copies current contents of output to backup file
-void clear_lineBuff(char * lineBuff);	//nullifies line buffer between each line of input
-void printToken(FILE *input, FILE *output,token tok[],int count, char *lineBuff); //prints the token to appropriate files
+int getInFile(int arg_count,  char * argv[],FILE **input); 					//Reads input and opens input file if exists
+int getOutFile(int arg_count,  char * argv[], FILE **output, FILE **listing, FILE **tmp); 	//Gets input and checks if output file exists
+void copy_backup(char *outFile);  								//Copies current contents of output to backup file
+void clear_lineBuff(char * lineBuff);								//Nullifies line buffer between each line of input
+void printToken(FILE *input, FILE *output,token tok[],int count, char *lineBuff);		//Prints the token to the appropriate files
 void ParseTokens(token tok[],int tokperline[]);
-void closeFiles(FILE *input, FILE *output, FILE *listing, FILE *tmp);   //closes all opened files
-void scanner (FILE *input, FILE *output, FILE *listing);				//reads line from input and calls functions to process line, continues until input eof
-int Tokenize( char *lineBuff,  token tok[]);							//breaks line into tokens calls functions to identify token type	
-void ident_token(int count, token tok[]);								//identifies token type and sets enumeration value of that token
-int checkReserved(token tmp);											//determines if token is a reserved word or variable
-int checkInt(token tmp);												//determines if token is an int
-int checkSymbol(token tmp);												// determines if token is a reserved symbol
+void closeFiles(FILE *input, FILE *output, FILE *listing, FILE *tmp);   			//Close all opened files
+void scanner (FILE *input, FILE *output, FILE *listing);					//Reads line from input and process line, continues until input eof
+int Tokenize( char *lineBuff,  token tok[]);							//Breaks line into tokens calls functions to identify token type	
+void ident_token(int count, token tok[]);							//Identifies token type and sets enumeration value of that token
+int checkReserved(token tmp);									//Check if token is a reserved word or variable
+int checkInt(token tmp);									//Check if token is an integer
+int checkSymbol(token tmp);									//Checks if token is a reserved symbol
 int BEGIN(FILE *listing, token tok[],int i, int tokperline[]);
 int STMTLIST(FILE *listing,token tok[], int i, int tokperline[]);
 int STMT(FILE *listing, token tok[], int i, int tokperline[]);
