cd3f1f27cd2e791187626b538691b83d1f11c003
Zidlan1O <45948180+Zidlan1O@users.noreply.github.com>
2019-03-21 10:40:14 -0400

    Update main.c

--- Prog 3.1/main.c
+++ Prog 3.1/main.c
@@ -1,10 +1,16 @@
-
+/*Program 3
+  CSC-460 Language Translation
+  Group 9
+  Chris Crisson CRI4537@calu.edu
+  Matthew Bedillion BED9714@calu.edu
+  Mark Blatnik BLA9072@calu.edu
+  Adlene Bellaoucha BEL7984@calu.edu
+*/
 
 #include "file_util.h"
 
 
 char inFile[MAX_CHAR], outFile[MAX_CHAR],tmpName[MAX_CHAR],c='\0',choice='\0';
-			// typedef to token for easier reference
 int tokNum=0,lexErrors=0,synErrors=0,counter=0;
 int main (int argc, char *argv[])
 {
@@ -12,11 +18,11 @@ int main (int argc, char *argv[])
          if(getInFile(argc, argv,&input));
 		{
 		 				
-			if(getOutFile(argc, argv, &output, &listing, &tmp)) //call function for output file opening
+			if(getOutFile(argc, argv, &output, &listing, &tmp))         //Calls the function to open output file 
 			{
 			
 				 scanner(input,output,listing);
-				 closeFiles( input,  output,  listing,  tmp); //call function to close any open files
+				 closeFiles( input,  output,  listing,  tmp);       //Calls the function to close the open files
 				 printf("\n There were %d tokens total",tokNum);
 				 printf("\n There were %d Lexical errors\n",lexErrors);
 				
@@ -24,7 +30,6 @@ int main (int argc, char *argv[])
 			
 		}
 	
-  printf("\n\tHAVE A NICE DAY \n");
   system ("Pause");
   return 0;
 }
